376a17a889dd088da35bb00ed45b38abedf9e198
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-03-01 13:39:39 +0200

    start

--- js/task-01.js
+++ js/task-01.js
@@ -6,3 +6,5 @@ categoriesList.forEach(item => {
     console.log(`Category: ${item.firstElementChild.textContent}`);
     console.log(`Elements: ${item.lastElementChild.children.length}`)
 });
+
+console.log(123);
