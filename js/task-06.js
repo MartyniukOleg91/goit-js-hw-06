ff92102b6e7f6b59351ef61a7ba26aeb5f21388d
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-03-01 14:24:14 +0200

    fix bug 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,10 @@
 
 const textInput = document.querySelector('#validation-input')
-console.log(textInput.getAttribute('data-length'))
+textInput.getAttribute('data-length')
+
+
 textInput.addEventListener('blur', event => {
+	textInput.classList.remove('valid', 'invalid')
 	if (event.target.value.length === Number(textInput.getAttribute('data-length'))) {
 		textInput.classList.add('valid')
 	} else {
